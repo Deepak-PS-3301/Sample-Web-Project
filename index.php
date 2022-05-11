24857f4b8a79b100369e9f9f78ea752dc56b4efe
Deepak PS <79774136+Deepak-PS-3301@users.noreply.github.com>
2022-05-11 15:33:38 +0530

    Updated files with php extension

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-2 x"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus, voluptatem aliquam quod iusto sed laboriosam porro voluptatum quam! Delectus fugit excepturi magni reiciendis autem placeat. Perspiciatis ducimus deleniti consequuntur blanditiis.</p></div>
+            <div class="col col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4 col-xxl-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam exercitationem beatae distinctio similique pariatur labore, at cumque incidunt doloremque eum ad iure maiores voluptate voluptas ipsa deserunt recusandae repellendus atque.</p></div>
+            <div class="col col-12 col-sm-2 col-md-4 col-lg-4 col-xl-4 col-xxl-4"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et illum incidunt aperiam soluta! Nesciunt, accusamus, alias odio facere obcaecati quibusdam dolores quaerat, consectetur cumque labore nisi distinctio fugit est incidunt.</p></div>
+        </div>
+    </div>
+</body>
+</html>
